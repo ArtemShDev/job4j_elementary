f4eb237c99c57403335cee4840f1942438bab15c
ArtemShDev <dev.art.balt@gmail.com>
2021-05-25 13:10:10 +0300

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Artem Shmelev");
+        System.out.println("01/03/1985");
+    }
+}
