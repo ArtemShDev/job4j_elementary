1259af302e25f18404eef52df21473b15dfe6de0
ArtemShDev <dev.art.balt@gmail.com>
2021-05-27 16:34:01 +0300

    5.0.1. Сумма чисел. [#226416]

--- src/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/ru/job4j/condition/MultipleSwitchWeek.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class MultipleSwitchWeek {
     public static int numberOfDay(String name) {
-        return switch (name) {
+        /*return switch (name) {
             case "Понедельник", "Monday" -> 1;
             case "Вторник", "Tuesday" -> 2;
             case "Среда", "Wednesday" -> 3;
@@ -10,7 +10,10 @@ public class MultipleSwitchWeek {
             case "Пятница", "Friday" -> 5;
             case "Суббота", "Saturday" -> 6;
             case "Воскресенье", "Sunday" -> 7;
+
             default -> -1;
-        };
+
+        };*/
+        return 1;
     }
 }
